58e53cb28594884b5efda5f6e53b5e34bb5f89f6
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-10-27 16:23:20 +0100

    changed domain object - Order

---
+++ src/main/java/pl/kupniewski/application/domain/Customer/Customer.java
@@ -0,0 +1,2 @@
+package pl.kupniewski.application.domain.Customer;public class Customer {
+}
--- src/main/java/pl/kupniewski/application/domain/Order/Order.java
+++ src/main/java/pl/kupniewski/application/domain/Order/Order.java
@@ -1,11 +1,20 @@
 package pl.kupniewski.application.domain.Order;
 
+import lombok.AllArgsConstructor;
+import pl.kupniewski.application.domain.Customer.Customer;
+import pl.kupniewski.application.model.Menu;
+import pl.kupniewski.application.model.Restaurant;
+import pl.kupniewski.application.model.RestaurantAddress;
+
+import java.sql.ResultSet;
+
+@AllArgsConstructor
 public class Order {
 
-    public Long createOrder(Long userId){
 
-        throw new UnsupportedOperationException("Not implemented yet!");
+    private Menu menu;
+    private Customer customer;
+    private Restaurant restaurant;
 
-    }
 
 }
