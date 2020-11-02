eaf5acab316c63c2ae06b3f4de86074b9eeff46b
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-02 17:48:44 +0100

    added dao restaurand findById method

--- src/main/java/pl/kupniewski/application/usecases/rustleup/RestaurantDao.java
+++ src/main/java/pl/kupniewski/application/usecases/rustleup/RestaurantDao.java
@@ -1,9 +1,12 @@
 package pl.kupniewski.application.usecases.rustleup;
 
+import pl.kupniewski.application.usecases.rustleup.model.MenuDto;
 import pl.kupniewski.application.usecases.rustleup.model.RestaurantDto;
+
+import java.util.List;
 
 public interface RestaurantDao {
 
-     RestaurantDto findRestauranById(Long restaurantId);
-
+    RestaurantDto findRestauranById(Long restaurantId);
+    MenuDto findPositionsById(List<Long> orderedPositions);
 }
