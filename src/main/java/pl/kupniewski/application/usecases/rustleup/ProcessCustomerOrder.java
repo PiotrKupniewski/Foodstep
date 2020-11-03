806c80f91106e2793bec766458d92dba438f07bc
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-03 09:38:01 +0100

    added methods to process customer oorder by restaurant

--- src/main/java/pl/kupniewski/application/usecases/rustleup/ProcessCustomerOrder.java
+++ src/main/java/pl/kupniewski/application/usecases/rustleup/ProcessCustomerOrder.java
@@ -1,10 +1,29 @@
 package pl.kupniewski.application.usecases.rustleup;
 
+import lombok.AllArgsConstructor;
+import pl.kupniewski.application.usecases.deliver.DeliverOrderToCustomer;
 import pl.kupniewski.application.usecases.order.model.OrderDto;
 
+@AllArgsConstructor
 public class ProcessCustomerOrder {
 
-    public OrderDto proceessOrderForCustomer() {
+    private final DeliverOrderToCustomer deliverOrderToCustomer;
+
+    public OrderDto assignCustomerOrderToRestaurant() {
+
+        updateOrderStatus(OrderDto.OrderStatus.IN_PROGRESS);
+        //prepare order
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    protected OrderDto assignOrderToDeliverer() {
+
+        deliverOrderToCustomer.assignOrderToDeliverer();
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    protected OrderDto updateOrderStatus(OrderDto.OrderStatus orderStatus) {
+        //send message to user after every order status update
         throw new UnsupportedOperationException("Not implemented yet!");
     }
 }
