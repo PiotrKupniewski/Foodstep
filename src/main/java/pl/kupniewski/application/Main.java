2a54132394895a8f36d46c96d5f639024d60c040
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-03 09:37:05 +0100

    added spring boot main class

--- src/main/java/pl/kupniewski/application/Main.java
+++ src/main/java/pl/kupniewski/application/Main.java
@@ -1,8 +1,15 @@
 package pl.kupniewski.application;
 
-public class Main {
-    public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+@ComponentScan(basePackages = "pl.kupniewski")
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
 
     }
 }
