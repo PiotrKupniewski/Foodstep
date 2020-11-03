d8a859e4cd56fc09c1cb8ba82c741565991b39f8
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-03 15:06:05 +0100

    imports updates

--- src/main/java/pl/kupniewski/application/usecases/order/CreateOrderByCustomer.java
+++ src/main/java/pl/kupniewski/application/usecases/order/CreateOrderByCustomer.java
@@ -2,12 +2,9 @@ package pl.kupniewski.application.usecases.order;
 
 import lombok.AllArgsConstructor;
 import pl.kupniewski.application.authenticate.UserDao;
-import pl.kupniewski.application.usecases.order.model.CustomerDto;
 import pl.kupniewski.application.usecases.order.model.OrderDto;
-import pl.kupniewski.application.usecases.rustleup.ProcessCustomerOrder;
+import pl.kupniewski.application.usecases.rustleup.ProcessCustomerOrderToRestaurant;
 import pl.kupniewski.application.usecases.rustleup.RestaurantDao;
-import pl.kupniewski.application.usecases.rustleup.model.MenuDto;
-import pl.kupniewski.application.usecases.rustleup.model.RestaurantDto;
 
 import java.util.List;
 import java.util.Optional;
