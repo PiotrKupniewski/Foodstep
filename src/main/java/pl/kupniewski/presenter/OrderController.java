5e56f7cb852a8a09080ada4c8db1f303ad96f36a
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-03 09:40:15 +0100

    added first htlm index to app

--- src/main/java/pl/kupniewski/presenter/OrderController.java
+++ src/main/java/pl/kupniewski/presenter/OrderController.java
@@ -1,2 +1,14 @@
-package pl.kupniewski.presenter;public class OrderController {
+package pl.kupniewski.presenter;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @GetMapping("/orderFood")
+    public String greeting(String name, Model model) {
+        return "order";
+    }
 }
