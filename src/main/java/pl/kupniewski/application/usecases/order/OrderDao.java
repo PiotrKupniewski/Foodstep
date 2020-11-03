0fdf4cdb8d19fadf007035cfe5e701e39d5d58b3
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-03 15:03:47 +0100

    added order dao impl

--- src/main/java/pl/kupniewski/application/usecases/order/OrderDao.java
+++ src/main/java/pl/kupniewski/application/usecases/order/OrderDao.java
@@ -1,2 +1,9 @@
-package pl.kupniewski.application.usecases.order;public interface OrderDao {
+package pl.kupniewski.application.usecases.order;
+
+import pl.kupniewski.application.usecases.order.model.OrderDto;
+
+import java.util.List;
+
+public interface OrderDao {
+    OrderDto createNewOrder(Long customerId, Long restaurantId, List<Long> items);
 }
--- src/main/java/pl/kupniewski/application/usecases/order/OrderDaoImpl.java
+++ src/main/java/pl/kupniewski/application/usecases/order/OrderDaoImpl.java
@@ -1,2 +1,14 @@
-package pl.kupniewski.application.usecases.order.model;public class OrderDaoImpl {
+package pl.kupniewski.application.usecases.order;
+
+
+import pl.kupniewski.application.usecases.order.model.OrderDto;
+
+import java.util.List;
+
+public class OrderDaoImpl implements OrderDao {
+
+    @Override
+    public OrderDto createNewOrder(Long customerId, Long restaurantId, List<Long> items) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
 }
