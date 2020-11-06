eac0ddddc8c1b7df688c595c9f5372938901bf67
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-06 14:19:05 +0100

    added prepared order model

--- src/main/java/pl/kupniewski/application/usecases/order/model/PreparedOrder.java
+++ src/main/java/pl/kupniewski/application/usecases/order/model/PreparedOrder.java
@@ -1,4 +1,11 @@
 package pl.kupniewski.application.usecases.order.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
 public class PreparedOrder {
+    private String status;
 }
