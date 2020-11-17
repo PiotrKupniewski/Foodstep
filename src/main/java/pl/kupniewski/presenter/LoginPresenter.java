a8a54fdaf1e02800cbd5b3415a94d348dc6d1669
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-17 10:47:31 +0100

    added login presenter

--- src/main/java/pl/kupniewski/presenter/LoginPresenter.java
+++ src/main/java/pl/kupniewski/presenter/LoginPresenter.java
@@ -1,2 +1,16 @@
-package pl.kupniewski.presenter;public class LoginPresenter {
+package pl.kupniewski.presenter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.kupniewski.application.authenticate.AuthenticateUser;
+
+@Controller
+public class LoginPresenter {
+
+    AuthenticateUser authenticateUser;
+
+    @GetMapping("/")
+    public String loginUser(){
+        return authenticateUser.getAuthenticatedViewName();
+    }
 }
