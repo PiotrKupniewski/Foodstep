52837fa31304edaf315bda9fe1410399c7c4335f
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-17 09:47:44 +0100

    added restaurant presenter and html view

--- src/main/java/pl/kupniewski/presenter/RestaurantController.java
+++ src/main/java/pl/kupniewski/presenter/RestaurantController.java
@@ -1,2 +1,14 @@
-package pl.kupniewski.presenter;public class RestaurantController {
+package pl.kupniewski.presenter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RestaurantController {
+
+    @GetMapping("/restaurant")
+    public String restaurantView() {
+        return "restaurant";
+    }
+
 }
