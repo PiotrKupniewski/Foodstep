c20b04b07c463cabbab90cbec5301748bc5f4c8d
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-17 10:46:44 +0100

    added user pojo

--- src/main/java/pl/kupniewski/application/authenticate/User.java
+++ src/main/java/pl/kupniewski/application/authenticate/User.java
@@ -1,2 +1,12 @@
-package pl.kupniewski.application.authenticate;public class User {
+package pl.kupniewski.application.authenticate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private String userLogin;
+    private UserRole role;
+
 }
