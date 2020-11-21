a74f72aa3767b80fc53afae67bfad7da53a33789
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-21 17:11:02 +0100

    changed positions of private methods

--- src/main/java/pl/kupniewski/application/usecases/authenticate/AuthenticateUser.java
+++ src/main/java/pl/kupniewski/application/usecases/authenticate/AuthenticateUser.java
@@ -7,12 +7,12 @@ public class AuthenticateUser {
 
     private final User user;
 
-    private User authenticate(){
-        return user;
-    }
-
     public String getAuthenticatedViewName(){
         User authenticatedUser = authenticate();
         return authenticatedUser.getRole() == UserRole.CUTOMER ? "customer" : "restaurant";
+    }
+
+    private User authenticate(){
+        return user;
     }
 }
