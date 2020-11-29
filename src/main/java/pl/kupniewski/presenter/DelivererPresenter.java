16e1a2ede1a86cf4e048f1ebad559d548652f7fe
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-29 20:22:31 +0100

    created deliverer presenter

--- src/main/java/pl/kupniewski/presenter/DelivererPresenter.java
+++ src/main/java/pl/kupniewski/presenter/DelivererPresenter.java
@@ -1,2 +1,20 @@
-package pl.kupniewski.presenter;public class DelivererPresenter {
+package pl.kupniewski.presenter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kupniewski.application.deliver.OrderReadyToDeliver;
+import pl.kupniewski.simulation.DelivererApi;
+
+@Controller
+@RequestMapping("/deliverer")
+public class DelivererPresenter {
+
+    DelivererApi delivererApi;
+
+    @GetMapping
+    public OrderReadyToDeliver deliverOrderToCustomer(@RequestBody OrderReadyToDeliver orderReadyToDeliver){
+        return delivererApi.deliverOrderToCustomer(orderReadyToDeliver);
+    }
 }
