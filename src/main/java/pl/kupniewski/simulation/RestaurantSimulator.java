d8208fe6e3e6ea5391f9a4a429136b89ea954ddf
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-12-01 09:22:13 +0100

    added restaurant simulator

--- src/main/java/pl/kupniewski/simulation/RestaurantSimulator.java
+++ src/main/java/pl/kupniewski/simulation/RestaurantSimulator.java
@@ -1,2 +1,17 @@
-package pl.kupniewski.simulation;public class RestaurantSimulator {
+package pl.kupniewski.simulation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import pl.kupniewski.application.order.Order;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@Getter
+@Component
+public class RestaurantSimulator {
+    private final List<Order> ordersList;
+
 }
