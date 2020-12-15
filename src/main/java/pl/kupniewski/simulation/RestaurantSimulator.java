221290562922286fe43045655fd71d992605b53d
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-12-15 20:19:37 +0100

    small refactor in restaurant simulator

--- src/main/java/pl/kupniewski/simulation/RestaurantSimulator.java
+++ src/main/java/pl/kupniewski/simulation/RestaurantSimulator.java
@@ -3,15 +3,16 @@ package pl.kupniewski.simulation;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
-import pl.kupniewski.application.order.Order;
-
-import java.util.List;
+import pl.kupniewski.application.deliver.api.RestaurantApi;
+import java.util.UUID;
 
 
 @AllArgsConstructor
 @Getter
 @Component
 public class RestaurantSimulator {
-    private final List<Order> ordersList;
+
+    private final RestaurantApi restaurantApi;
+    private final UUID uuid = UUID.randomUUID();
 
 }
