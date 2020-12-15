f11da73e2c30804e58e1c88bd38174c67a47e8b0
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-12-15 19:11:14 +0100

    added static import to order status

--- src/main/java/pl/kupniewski/application/order/CreateOrderByCustomer.java
+++ src/main/java/pl/kupniewski/application/order/CreateOrderByCustomer.java
@@ -3,6 +3,7 @@ package pl.kupniewski.application.order;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.kupniewski.application.deliver.api.RestaurantApi;
+import pl.kupniewski.application.order.Order.Status;
 
 @AllArgsConstructor
 @Component
@@ -11,7 +12,7 @@ public class CreateOrderByCustomer {
     RestaurantApi restaurant;
 
     public Order createNewOrderForCustomer() {
-        Order order = new Order(1L, Order.Status.IN_PROGRESS);
+        Order order = new Order(1L, Status.IN_PROGRESS);
         restaurant.addOrderToRestaurantList(order);
         return order;
     }
