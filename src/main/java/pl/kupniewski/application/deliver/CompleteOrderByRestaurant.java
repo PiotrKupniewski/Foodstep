df8a31ce984631f6ecec131197cc46530dab407b
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2021-01-04 23:08:39 +0100

    added empty constructor, need refactor with json property in next steps

--- src/main/java/pl/kupniewski/application/deliver/CompleteOrderByRestaurant.java
+++ src/main/java/pl/kupniewski/application/deliver/CompleteOrderByRestaurant.java
@@ -1,10 +1,12 @@
 package pl.kupniewski.application.deliver;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.kupniewski.application.deliver.api.DelivererApi;
 import pl.kupniewski.application.order.Order;
 
 @Component
+@AllArgsConstructor
 public class CompleteOrderByRestaurant {
 
     DelivererApi delivererApi;
--- src/main/java/pl/kupniewski/application/order/Order.java
+++ src/main/java/pl/kupniewski/application/order/Order.java
@@ -2,15 +2,17 @@ package pl.kupniewski.application.order;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Order {
 
     private final UUID orderId = UUID.randomUUID();
-    private final Status status;
+    private Status status;
 
     public enum Status {
         IN_PROGRESS, FINISHED
