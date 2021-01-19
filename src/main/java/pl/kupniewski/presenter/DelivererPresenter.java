9265a04bc6969ea6fc4c778d6ef148e4f52ebe60
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2021-01-19 18:38:24 +0100

    added deliverer resenter

--- src/main/java/pl/kupniewski/presenter/DelivererPresenter.java
+++ src/main/java/pl/kupniewski/presenter/DelivererPresenter.java
@@ -1,20 +1,37 @@
 package pl.kupniewski.presenter;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kupniewski.application.deliver.OrderReadyToDeliver;
 import pl.kupniewski.application.deliver.api.DelivererApi;
 
-@Controller
+import java.util.Map;
+import java.util.UUID;
+
+@Controller@AllArgsConstructor
 @RequestMapping("/deliverer")
 public class DelivererPresenter {
 
     DelivererApi delivererApi;
 
-    @GetMapping
-    public OrderReadyToDeliver deliverOrderToCustomer(@RequestBody OrderReadyToDeliver orderReadyToDeliver){
-        return delivererApi.deliverOrderToCustomer(orderReadyToDeliver);
+    @GetMapping("")
+    public String restaurantView() {
+        return "deliverer";
+    }
+
+    @GetMapping("/deliverOrder")
+    public ResponseEntity<OrderReadyToDeliver> deliverOrderToCustomer(@RequestBody OrderReadyToDeliver orderReadyToDeliver) {
+        return new ResponseEntity<>(delivererApi.deliverOrderToCustomer(orderReadyToDeliver), HttpStatus.OK);
+    }
+
+    @GetMapping("/getActiveOrders")
+    public ResponseEntity<Map<UUID, OrderReadyToDeliver>> getActiveDelivererOrders() {
+            return new ResponseEntity<>(delivererApi.getActiverders(), HttpStatus.OK);
     }
 }
