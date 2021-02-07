9eccb5944ba20f8ecb836c86c138f5b418fe188c
p.kupniewski <pkupniewski.dev@gmail.com>
2021-02-07 20:57:25 +0100

    deliver order js methods implemented

--- src/main/resources/static/js/deliverer.js
+++ src/main/resources/static/js/deliverer.js
@@ -5,14 +5,22 @@ let spanDeliverer = document.getElementById('info');
 let ordersDiv = document.getElementById('orders');
 let readyOrders =[];
 
-let createOdersGrid = (orders) =>  {
+let createOrdersGrid = (orders) =>  {
+
+    removeCurrentList();
+    spanDeliverer.innerText = "";
+    if(orders.length === 0){
+        spanDeliverer.innerText = "No orders selected";
+        return;
+    }
 
     orders.forEach(x => {
         let paragraph = document.createElement('p');
         let radio = document.createElement('input');
-        radio.setAttribute('type', 'radio');
-        radio.setAttribute('name', 'choice');
-        radio.setAttribute('is', x.order.orderId);
+        radio.setAttribute('type', 'checkbox');
+        radio.setAttribute('name', 'order');
+        radio.setAttribute('id', x.order.orderId);
+        radio.setAttribute('value', x.order.orderId);
 
         let label = document.createElement('label');
         label.setAttribute('for', x.order.orderId)
@@ -27,32 +35,65 @@ let createOdersGrid = (orders) =>  {
 
 }
 
-getReadyOrdersButton.addEventListener("click", () => {
-
-    fetch('http://localhost:8080/deliverer/getActiveOrders' ,{
+function getActiveOrders() {
+    fetch('http://localhost:8080/deliverer/getActiveOrders', {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
-    }
-
+            'Content-Type': 'application/json',
+        }
     })
         .then(response => response.json())
         .then(data => readyOrders = data)
-        .then(data => createOdersGrid(readyOrders))
+        .then(() => createOrdersGrid(readyOrders))
         .catch(error => console.log('error:', error));
+}
+
+getReadyOrdersButton.addEventListener("click", () => {
+    getActiveOrders();
 });
 
+function removeCurrentList() {
+    while (ordersDiv.firstChild) {
+        ordersDiv.removeChild(ordersDiv.firstChild);
+    }
+}
+
+function buildSelectedOrderObjects() {
+    let checkedOrders = document.querySelectorAll('input[name="order"]:checked')
+    let deliveredOrdersArr = [];
+
+    checkedOrders.forEach(x => {
+        deliveredOrdersArr.push(
+            {
+                status:"FINISHED",
+                    order: {
+                        orderId : x.value,
+                        status: "SEND_TO_DELIVERER"
+                    }
+            })
+    });
+
+    console.log(deliveredOrdersArr);
+    return deliveredOrdersArr.length > 0 ? deliveredOrdersArr : null;
+}
+
 markAsDelivered.addEventListener("click", () => {
+
+    let checkedOrders = document.querySelectorAll('input[name="order"]:checked')
+    spanDeliverer.innerText = "";
+    if(checkedOrders.length === 0 ){
+        spanDeliverer.innerText = "No orders selected";
+        return;
+    }
 
     fetch('http://localhost:8080/deliverer/deliverOrder',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(readyOrders[0])
+        body: JSON.stringify(buildSelectedOrderObjects()),
         })
-        .then(response => response.json())
-        .then(data => spanDeliverer.innerText = JSON.stringify(data))
+        .then(() => getActiveOrders())
         .catch(error => console.log('error:', error));
 });
 
