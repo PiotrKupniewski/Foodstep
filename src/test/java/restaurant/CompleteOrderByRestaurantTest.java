b6ba5e8fbc74d4c5e590eda3af404ad54fe13639
p.kupniewski <pkupniewski.dev@gmail.com>
2021-02-07 16:32:46 +0100

    added junit5 dependencies and initial test

--- src/test/java/restaurant/CompleteOrderByRestaurantTest.java
+++ src/test/java/restaurant/CompleteOrderByRestaurantTest.java
@@ -1,7 +1,13 @@
-package pl.kupniewski.application.restaurant;
+package restaurant;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompleteOrderByRestaurantTest {
 
+    @Test
+    void test() {
+        System.out.println("test");
+        assertTrue(true);
+    }
 }
